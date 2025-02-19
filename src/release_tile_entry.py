cb8c27cbd4f9dac16239ef24dfe19ae54ebf217e
shettigarnishan <shettigarn@hhmi.org>
2025-02-19 16:04:05 -0500

    releases_entry notebook to script

---
+++ src/release_tile_entry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python 3
+#  -*- coding: utf8 -*-
+
+"""
+inputs:
+
+optional_inputs:
+
+outputs:
+
+optional_outputs:
+
+created: 20250219
+author:ns
+
+"""
+
+if __name__ == "__main__":
+    #imports
+    #sysarvs
+    #outputs
